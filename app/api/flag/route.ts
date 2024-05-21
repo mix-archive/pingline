462617a123e9282457f77c7ba5ca96e58e179c17
Mix <32300164+mnixry@users.noreply.github.com>
2024-05-21 11:29:55 +0800

    chore: Reformat the code with Prettier

--- app/api/flag/route.ts
+++ app/api/flag/route.ts
@@ -1,14 +1,16 @@
-import { NextResponse } from 'next/server';
-
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-    try {
-        const {balance, flag} = await req.json();
-        console.log(`Received balance = ${balance}, flag = ${flag}`);
+  try {
+    const { balance, flag } = await req.json();
+    console.log(`Received balance = ${balance}, flag = ${flag}`);
 
-        return NextResponse.json({ message: 'Received your flag!' });
-    } catch {
-        console.log('Got a malformed request :(')
-        return NextResponse.json({ message: 'An error occurred...' }, { status: 500 })
-    }
+    return NextResponse.json({ message: "Received your flag!" });
+  } catch {
+    console.log("Got a malformed request :(");
+    return NextResponse.json(
+      { message: "An error occurred..." },
+      { status: 500 },
+    );
+  }
 }
