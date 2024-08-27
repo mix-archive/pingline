906494bcf5bb389a392aa56bf06e3b3f34bf4910
Mix <32300164+mnixry@users.noreply.github.com>
2024-08-27 19:28:44 +0800

    chore: Refactor entrypoint.sh to allow monitor local file change

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,30 @@
 #!/bin/bash -xe
 
-if [ -z "$FLAG" ]; then
-    FLAG="flag{test}"
-fi
+RELOAD_LOCK_PATH=${RELOAD_LOCK_PATH:-/tmp/.reload-lock}
+FLAG=${FLAG:-flag{this_is_a_test_flag}}
 
 echo -n $FLAG > /flag
 chmod 444 /flag
 unset FLAG
 
-if [ -f "./.next" ]; then
-    rm -rf ./.next
-fi
+function build_and_start() {
+    if [ -f "./.next" ]; then
+        rm -rf ./.next
+    fi
+    su app -c 'pnpm build'
 
-su app -c 'pnpm build'
+    while [ ! -f $RELOAD_LOCK_PATH ]; do
+        su app -c \
+            'exec timeout 30s pnpm start' || true
+    done
+    rm -vf $RELOAD_LOCK_PATH
+}
 
-while true; do
-    su app -c \
-        'exec timeout 30s pnpm start' || true
+build_and_start &
+
+while inotifywait -e modify,create,delete -r ./app; do
+    echo "File change detected. Rebuilding..."
+    touch $RELOAD_LOCK_PATH
+    su app -c 'kill -TERM -1'
+    build_and_start &
 done
